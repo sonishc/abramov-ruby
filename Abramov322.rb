fe24320fa947eafdcf84239c9e4784e94b43fa54
sonishc <onixsonic@gmail.com>
2017-08-30 10:53:14 +0300

    Add complete 3 task Abramov book

---
+++ Abramov322.rb
@@ -0,0 +1,19 @@
+################################################# 
+# Returns a MAX SUM DIVIDES numbers within all natural
+# numbers of range (1..to user_Number) 
+# As example n_1 = 36 (1+2+3+4+6+9+12+18) = 55
+#            n_2 = 5  (1) = 1
+# max = 55
+################################################# 
+
+puts "Write last number of range"
+
+n = gets.to_i           # Get user data
+numbers = (1..n).to_a   # Convert range to array
+
+arr = numbers.map do |number| # Create result array witch contains sum divedes
+    a = (1..number).select {|n| number % n == 0 and n != number}
+    a.inject(0){|sum,x| sum + x }
+end
+
+puts arr.inject {|m,x| m>x ? m : x} # Get max element in result array
